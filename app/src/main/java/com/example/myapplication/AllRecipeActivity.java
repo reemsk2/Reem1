9dbfde6f21e20e0d313bf9d5846e0e7ddb5aafcf
Reem <reem.skh2004@gmail.com>
2021-12-04 13:48:20 +0200

    initial commit

---
+++ app/src/main/java/com/example/myapplication/AllRecipeActivity.java
@@ -0,0 +1,57 @@
+package com.example.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.AbsListView;
+import android.widget.Adapter;
+import android.widget.LinearLayout;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class AllRecipeActivity extends AppCompatActivity {
+    private RecyclerView rvAllRecipe;
+    AdapterRecipe adapter;
+    FirebaseServices fbs;
+    ArrayList<Recipe> recipes;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_recipe);
+        fbs = FirebaseServices.getInstance();
+        recipes = new ArrayList<Recipe>();
+        readData();
+
+        RecyclerView recyclerView = findViewById(R.id.rvRecipesAllRecipe);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new AdapterRecipe(this , recipes);
+        recyclerView.setAdapter(adapter);
+    }
+    private void readData() {
+        fbs.getFirestore().collection("recipes")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                recipes.add(document.toObject(Recipe.class));
+                            }
+                        } else {
+                            Log.e("AllRestActivity: readData()", "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+    }
+}
