1afc856c4d25ecb3b8e082cb081da0e8c731f7e3
Reem <reem.skh2004@gmail.com>
2022-02-15 10:24:31 +0200

    gradle  and details

--- app/src/main/java/com/example/myapplication/RecipeDetailsActivity.java
+++ app/src/main/java/com/example/myapplication/RecipeDetailsActivity.java
@@ -1,14 +1,45 @@
 package com.example.myapplication;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.google.firebase.storage.StorageReference;
 
 public class RecipeDetailsActivity extends AppCompatActivity
 {
+    private EditText etName, etDesc, etAddress, etPhone;
+    private Spinner spFoods;
+    private ImageView ivPhoto;
+    private FirebaseServices fbs;
+    StorageReference storageReference;
+    private Uri filePath;
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_recipe);
-    }
 
+        connectComponents();
+        Intent i = this.getIntent();
+        Recipe recipe = (Recipe) i.getSerializableExtra("recipe");
+
+        etName.setText(Recipe.getName());
+        etDesc.setText(Recipe.getName());
+        etAddress.setText(Recipe.getName());
+        etPhone.setText(Recipe.getName());
+        Picasso.get().load(Recipe.getPhoto()).into(ivPhoto);
+    }
+    private void connectComponents() {
+        etName = findViewById(R.id.tvNameRecipeDetails);
+        etDesc = findViewById(R.id.etDescRecipeDetails);
+        etAddress = findViewById(R.id.etAddressRecipeDetails);
+        etPhone = findViewById(R.id.etPhoneRecipeDetails);
+        ivPhoto = findViewById(R.id.etPhotoRecipeDetails);
 }
+}
+
